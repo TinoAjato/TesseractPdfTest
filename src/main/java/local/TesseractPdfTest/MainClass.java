d07522e518b92d9d67124d664ec540abbcb3a377
beliaev <beliaev@WS-5609.pro.vssp.by>
2021-12-10 14:46:11 +0300

    подправил координаты, удалил лишние строки

--- src/main/java/local/TesseractPdfTest/MainClass.java
+++ src/main/java/local/TesseractPdfTest/MainClass.java
@@ -37,12 +37,12 @@ public class MainClass {
 	private static List<ConsignmentNoteAfterBatchProcessing> consignmentNotesAbp = new ArrayList<ConsignmentNoteAfterBatchProcessing>();
 	
 	/**портретная ориентация*/
-	private static Rectangle standardStampArea_p = new Rectangle(180, 375, 2150, 255);
+	private static Rectangle standardStampArea_p = new Rectangle(180, 375, 1021, 255);
 	private static Rectangle unpArea_p = new Rectangle(885, 150, 965, 230);
 	private static Rectangle requisitesArea_p = new Rectangle(0, 595, 2300, 495);
 	
 	/**альбомная ориентация*/
-	private static Rectangle standardStampArea_l = new Rectangle(180, 265, 3150, 280);
+	private static Rectangle standardStampArea_l = new Rectangle(180, 265, 2041, 280);
 	private static Rectangle unpArea_l = new Rectangle(1375, 80, 1100, 200);
 	private static Rectangle requisitesArea_l = new Rectangle(0, 470, 3300, 350);
 	
@@ -50,15 +50,11 @@ public class MainClass {
 	private static Rectangle rectangleSeries_p = new Rectangle(0, 0, 260, 110);
 	//прямоугольник наименования типа
 	private static Rectangle typeNameRectangle_p = new Rectangle(220, 55, 800, 160);
-////	//прямоугольник номера штрихкода
-//	private static Rectangle barcodeNumberRectangle_p = new Rectangle(1060, 125, 510, 110);
 	
 	//прямоугольник серии
 	private static Rectangle rectangleSeries_l = new Rectangle(0, 0, 260, 110);
 	//прямоугольник наименования типа
 	private static Rectangle typeNameRectangle_l = new Rectangle(220, 70, 1820, 160);
-////	//прямоугольник номера штрихкода
-//	private static Rectangle barcodeNumberRectangle_l = new Rectangle(2070, 160, 510, 110);
 	
 	
 	public static void main(String[] args) {
@@ -83,7 +79,7 @@ public class MainClass {
 			tesseract.setOcrEngineMode(2);
 			tesseract.setTessVariable("user_defined_dpi", "300");
 			
-			int lastPage = 3;//document.getNumberOfPages();
+			int lastPage = document.getNumberOfPages();
 			
 			for (int page = 0; page < lastPage; page++) {
 				try {
@@ -110,7 +106,6 @@ public class MainClass {
 						
 						coordsInStandardStamp.add(rectangleSeries_p);
 						coordsInStandardStamp.add(typeNameRectangle_p);
-//						coordsInStandardStamp.add(barcodeNumberRectangle_p);
 					} else {
 						standardStampArea = standardStampArea_l;
 						unpArea = unpArea_l;
@@ -118,7 +113,6 @@ public class MainClass {
 						
 						coordsInStandardStamp.add(rectangleSeries_l);
 						coordsInStandardStamp.add(typeNameRectangle_l);
-//						coordsInStandardStamp.add(barcodeNumberRectangle_l);
 					}
 					
 					try {
@@ -160,14 +154,6 @@ public class MainClass {
 						} else {
 							cnAbp.setConsignmentSeries("");
 						}
-						
-						/*ШТРИХКОД
-						String barcode = tesseract.doOCR(partOfBufferedImage1, coordsInStandardStamp.get(2));
-						barcode = barcode.replaceAll("[^\\d]", "");
-						if(barcode.length() > 0)
-							System.out.println("Штрихкод: " + barcode);
-						else
-							System.out.println("Штрихкод: отсутствует");*/
 						
 						//обводка областей стандартного штампа, где идет определение
 						for(Rectangle coordInStandardStamp : coordsInStandardStamp)
